3d75d66cf7d1637c26c6922e771c7a245d85948c
lucamilion <lucamilion@gmail.com>
2022-05-17 22:27:33 -0600

    Learned how to write unit tests in C#

---
+++ Routing Exercise Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Routing Exercise Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Routing Exercise Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f125a2e1-9a9e-427f-9136-b5707f9cfdd9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Routing Exercise Tests/TestTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Routing_Exercise_Tests
+{
+    [TestClass]
+    public class TestTest
+    {
+        [TestMethod]
+        public void TestPass()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestFail()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
